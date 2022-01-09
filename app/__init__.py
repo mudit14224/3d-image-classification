bdbae62d7900465e692e770f72b7f7cafdae7622
mudit14224 <mudit.jindal14224@gmail.com>
2022-01-09 12:11:35 +0530

    connected model to flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.config['UPLOAD_PATH'] = 'app/static'
+app.config['UPLOAD_PATH'] = 'app/static/uploads'
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,36 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask.templating import render_template
 from flask import request
 from werkzeug.utils import secure_filename
 from app import app
+import tensorflow as tf
+from preprocess import ProcessScan
+import os
+import numpy as np
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template("index.html")
+    path = None
+    text = None
+    results = [None, None]
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        f.save(path)
+        process_mod = ProcessScan()
+        scan = process_mod.process_scan(path)
+        text = "The scan has been processed successfully!"
+        model = tf.keras.models.load_model('./3d-img-class-model.h5', compile=False)
+        prediction = model.predict(np.expand_dims(scan, axis=0))[0]
+        scores = [1 - prediction[0], prediction[0]]
+        class_names = ['normal', 'abnormal']
+        results = []
+        for score, name in zip(scores, class_names):
+            results.append("This model is %.2f percent confident that CT scan is %s"% ((100 * score), name))
+
+    return render_template("index.html", path=path, text=text, res1=results[0], res2=results[1])
 
 @app.route('/dev')
 def dev_page():
